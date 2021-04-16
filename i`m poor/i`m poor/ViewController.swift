b170dbaf3353f7f941a678925bba3918b351eff0
Oleksii Bomersbah <82603311+4oxx@users.noreply.github.com>
2021-04-16 12:44:46 +0300

    Add files via upload

---
+++ i`m poor/i`m poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i`m poor
+//
+//  Created by user21 on 16.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i`m poor/i`m poorTests/i_m_poorTests.swift
@@ -0,0 +1,33 @@
+//
+//  i_m_poorTests.swift
+//  i`m poorTests
+//
+//  Created by user21 on 16.04.2021.
+//
+
+import XCTest
+@testable import i_m_poor
+
+class i_m_poorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
